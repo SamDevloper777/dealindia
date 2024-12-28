2feb424168a1374122b0fc0a5e5c96009f8e0ab1
roni836 <ronitsaha836@gmail.com>
2024-12-28 08:54:21 +0530

    tailwind intregated, database added, Ui Updated

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+   public function dashboard(){
+    return view('Admin.dashboard');
+   }
+
+   public function applications(){
+    return view('Admin.applications');
+   }
+}
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,24 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('user.home');
 });
+
+
+Route::get('/admin', function () {
+    return view('Admin.dashboard');
+});
+
+Route::get('/login', function () {
+    return view('auth.login');
+});
+
+Route::get('/register', function () {
+    return view('auth.register');
+});
+
+// Route::post('/register',[UserController::class,"adminRegister"])->name("admin.register");
+
+// Route::match(['get',"post"],'/admin-login',[UserController::class,"adminLogin"])->name("login");
+
+// Route::post('/logout',[UserController::class,"logout"])->name("logout");
